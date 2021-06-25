57a14e6ceffd6d92347299771345d289da29e970
Aaron <aaronwenteler@gmail.com>
2021-06-25 23:35:36 +0200

    Update binance tracker

--- binance_tracker.py
+++ binance_tracker.py
@@ -12,8 +12,8 @@ class BinanceFunctions:
     def fetch_binance_portfolio(self):
         portfolio = self.client.get_account_snapshot(type='SPOT')
 
-        print(portfolio['snapshotVos'][0])
-        print(portfolio['snapshotVos'][1])
-        print(portfolio['snapshotVos'][2])
-        print(portfolio['snapshotVos'][3])
-        print(portfolio['snapshotVos'][4])
+        print(portfolio)
+        # print(portfolio['snapshotVos'][1])
+        # print(portfolio['snapshotVos'][2])
+        # print(portfolio['snapshotVos'][3])
+        # print(portfolio['snapshotVos'][4])
